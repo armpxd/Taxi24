bbdf62ed72109d133d6241559de20faa8edc6c55
Alvaro <armpmartinez@gmail.com>
2024-06-12 21:21:04 -0400

    feat: Creating scalfolding controllers, services, modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,28 @@ import { Module } from '@nestjs/common';
 import { DriverModule } from './driver/driver.module';
 import { DbModule } from './db/db.module';
 import { ConfigModule } from '@nestjs/config';
+import { PersonModule } from './person/person.module';
+import { PassengerModule } from './passenger/passenger.module';
+import { LocationService } from './location/location.service';
+import { LocationController } from './location/location.controller';
+import { LocationModule } from './location/location.module';
+import { InvoiceModule } from './invoice/invoice.module';
+import { RideService } from './ride/ride.service';
+import { RideController } from './ride/ride.controller';
+import { RideModule } from './ride/ride.module';
 
 @Module({
-  imports: [DriverModule, DbModule, ConfigModule.forRoot({ isGlobal: true })],
-  controllers: [],
-  providers: [],
+  imports: [
+    DriverModule,
+    DbModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    PersonModule,
+    PassengerModule,
+    LocationModule,
+    InvoiceModule,
+    RideModule,
+  ],
+  controllers: [LocationController, RideController],
+  providers: [LocationService, RideService],
 })
 export class AppModule {}
--- src/driver/driver.service.ts
+++ src/driver/driver.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { EntityManager, Repository } from 'typeorm';
+import { Driver } from './entities/driver.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class DriverService {}
+export class DriverService {
+  constructor(
+    @InjectRepository(Driver)
+    private readonly driverRepository: Repository<Driver>,
+    private readonly entityManager: EntityManager,
+  ) {}
+}
