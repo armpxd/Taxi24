4ee1e27b9e85c902a83f48c8d281781900d0aa4d
Alvaro <armpmartinez@gmail.com>
2024-06-14 17:47:02 -0400

    fix: improving name

--- src/passenger/dto/getPassenger.dto.ts
+++ src/passenger/dto/getPassenger.dto.ts
@@ -3,8 +3,7 @@ import { Passenger } from '../entities/passenger.entity';
 export class GetPassengerDto {
   constructor(passenger: Passenger) {
     this.id = passenger.id;
-    this.name = passenger.person.name;
-    this.lastName = passenger.person.lastName;
+    this.name = `${passenger.person.name} ${passenger.person.lastName}`;
     this.phoneNumber = passenger.person.phoneNumber;
     this.email = passenger.person.email;
   }
