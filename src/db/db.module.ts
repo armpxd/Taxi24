57ea4db7dca769433d88e720cc884211e75c544f
Alvaro <armpmartinez@gmail.com>
2024-06-13 21:34:39 -0400

    fix: fixing db setting

--- src/db/db.module.ts
+++ src/db/db.module.ts
@@ -1,9 +1,28 @@
-import { Module } from "@nestjs/common";
-import { DbConnection } from "./typeorm.config";
+// src/modules/db.module.ts
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-    providers:[...DbConnection],
-    exports:[...DbConnection]
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('PG_HOST'),
+        port: configService.get('PG_PORT'),
+        username: configService.get('PG_USERNAME'),
+        password: configService.get('PG_PASSWORD'),
+        database: configService.get('DATABASE_NAME'),
+        synchronize: true, // Sync should be carefully used in production
+        logging: configService.get('ENVIRONMENT') === 'development',
+        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+        migrations: [__dirname + '/../db/migrations/*{.ts,.js}'],
+      }),
+    }),
+  ],
+  exports: [TypeOrmModule],
 })
-
-export class DbModule{}
+export class DbModule {}
