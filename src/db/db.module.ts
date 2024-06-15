7ab6e600676532cb502fb63a373168930334c680
Alvaro <armpmartinez@gmail.com>
2024-06-15 11:54:15 -0400

    fix: refactory db config

--- src/db/db.module.ts
+++ src/db/db.module.ts
@@ -2,26 +2,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { dataSourceOptions } from './dataSource';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get('PG_HOST'),
-        port: configService.get('PG_PORT'),
-        username: configService.get('PG_USERNAME'),
-        password: configService.get('PG_PASSWORD'),
-        database: configService.get('DATABASE_NAME'),
-        synchronize: true, // Sync should be carefully used in production
-        logging: configService.get('ENVIRONMENT') === 'development',
-        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-        migrations: [__dirname + '/../db/migrations/*{.ts,.js}'],
-      }),
-    }),
+    TypeOrmModule.forRoot(dataSourceOptions)
   ],
   exports: [TypeOrmModule],
 })
