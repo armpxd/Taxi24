4d1529e8376a625d6279b81b77527cfe39ce4d61
Alvaro <armpmartinez@gmail.com>
2024-06-15 16:11:49 -0400

    feat: adding invoice test

--- src/invoice/invoice.controller.spec.ts
+++ src/invoice/invoice.controller.spec.ts
@@ -1,18 +1,99 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { InvoiceController } from './invoice.controller';
+import { Passenger } from '../passenger/entities/passenger.entity';
+import { Location } from '../location/entities/location.entity';
+import { Driver } from '../driver/entities/driver.entity';
+import { Invoice } from './entities/invoice.entity';
+import { InvoiceService } from './invoice.service';
 
-describe('InvoiceController', () => {
+const location: Location = { id: 1, latitude: 40, longitude: 50 };
+const passenger: Passenger = {
+  id: 1,
+  person: {
+    id: 2,
+    name: 'person',
+    email: 'person@gmail.com',
+    lastName: 'marcos',
+    phoneNumber: '+1 809 324 3234',
+  },
+};
+const driver: Driver = {
+  id: 1,
+  person: {
+    id: 1,
+    name: 'driver',
+    email: 'driver@gmail.com',
+    phoneNumber: '+1 809 324 3234',
+    lastName: 'Lora',
+  },
+  available: true,
+  location,
+};
+
+const invoiceResult: Invoice[] = [
+  {
+    id: 1,
+    date: new Date(),
+    amount: 50,
+    ride: {
+      id: 1,
+      passenger,
+      driver,
+      startLocation: location,
+      endLocation: location,
+      status: 'finished',
+    },
+  },
+];
+
+class InvoiceServiceMock {
+  findAll() {
+    return invoiceResult;
+  }
+
+  findOneById() {
+    return invoiceResult[0];
+  }
+}
+
+describe('InvoicesController', () => {
   let controller: InvoiceController;
+  let service: InvoiceService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [InvoiceController],
+      providers: [
+        {
+          provide: InvoiceService,
+          useClass: InvoiceServiceMock,
+        },
+      ],
     }).compile();
 
     controller = module.get<InvoiceController>(InvoiceController);
+    service = module.get<InvoiceService>(InvoiceService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  describe('findAll', () => {
+    it('should return an array of invoices', async () => {
+      const result = invoiceResult;
+      jest.spyOn(service, 'findAll').mockResolvedValue(result);
+
+      expect(await controller.findAll()).toBe(result);
+    });
+  });
+
+  // describe('findOneById', () => {
+  //   it('should return a single invoice by ID', async () => {
+  //     const id = '1';
+  //     jest.spyOn(service, 'findOneById').mockResolvedValue(invoiceResult[0]);
+
+  //     expect(await controller.findOneById(id)).toBe(invoiceResult[0]);
+  //   });
+  // });
 });
--- src/invoice/invoice.service.spec.ts
+++ src/invoice/invoice.service.spec.ts
@@ -1,18 +1,128 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { InvoiceService } from './invoice.service';
+import { NotFoundException } from '@nestjs/common';
+import { Passenger } from '../passenger/entities/passenger.entity';
+import { Location } from '../location/entities/location.entity';
+import { Driver } from '../driver/entities/driver.entity';
+import { Invoice } from './entities/invoice.entity';
+import { Repository } from 'typeorm';
+import { getRepositoryToken } from '@nestjs/typeorm';
 
-describe('InvoiceService', () => {
+const location: Location = { id: 1, latitude: 40, longitude: 50 };
+const passenger: Passenger = {
+  id: 1,
+  person: {
+    id: 2,
+    name: 'person',
+    email: 'person@gmail.com',
+    lastName: 'sacha',
+    phoneNumber: '+1 809 386 0005',
+  },
+};
+const driver: Driver = {
+  id: 1,
+  person: {
+    id: 1,
+    name: 'driver',
+    email: 'driver@gmail.com',
+    lastName: 'asd',
+    phoneNumber: '+1 809 386 0005',
+  },
+  available: true,
+  location,
+};
+
+const invoiceResult: Invoice[] = [
+  {
+    id: 1,
+    date: new Date(),
+    amount: 50,
+    ride: {
+      id: 1,
+      passenger: {
+        id: 1,
+        person: {
+          id: 2,
+          name: 'person',
+          lastName: 'sacha',
+          email: 'person@gmail.com',
+          phoneNumber: '+1 809 386 0005',
+        },
+      },
+      driver: {
+        id: 1,
+        available: true,
+        person: {
+          id: 1,
+          name: 'driver',
+          lastName: 'asd',
+          email: 'driver@gmail.com',
+          phoneNumber: '+1 809 386 0005',
+        },
+        location: {
+          id: 1,
+          latitude: 40,
+          longitude: 50,
+        },
+      },
+      startLocation: {
+        id: 1,
+        latitude: 40,
+        longitude: 50,
+      },
+      endLocation: {
+        id: 1,
+        latitude: 40,
+        longitude: 50,
+      },
+      status: 'finished',
+    },
+  },
+];
+
+describe('InvoicesService', () => {
   let service: InvoiceService;
+  let repository: Repository<Invoice>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [InvoiceService],
+      providers: [
+        InvoiceService,
+        {
+          provide: getRepositoryToken(Invoice),
+          useClass: Repository,
+        },
+      ],
     }).compile();
 
     service = module.get<InvoiceService>(InvoiceService);
+    repository = module.get<Repository<Invoice>>(getRepositoryToken(Invoice));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('findAll', () => {
+    it('should return an array of invoices', async () => {
+      const mockInvoices = invoiceResult;
+      jest.spyOn(repository, 'find').mockResolvedValue(mockInvoices);
+
+      expect(await service.findAll()).toBe(mockInvoices);
+    });
+  });
+
+  describe('findOneById', () => {
+    it('should throw NotFoundException if invoice with given ID is not found', async () => {
+      const id = 99;
+      jest.spyOn(repository, 'findOne').mockResolvedValue(null);
+
+      try {
+        await service.findOneById(id);
+      } catch (error) {
+        expect(error).toBeInstanceOf(NotFoundException);
+        expect(error.message).toBe(`No invoice with the ${id}`);
+      }
+    });
   });
 });
